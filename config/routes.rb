4ae5925820b0e07d445448bbd8a01fd2de6a7ec2
pjchender <pjchender@gmail.com>
2018-10-09 16:28:35 +0800

    Add routes for talks_show

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
   get '/talks', to: 'pages#talks'
+  get '/talks/香港教育大學英語教育系-彭于榛-barbie-peng-ov-study-bde-hk', to: 'pages#talks_show', as: :talks_show
   get '/guides', to: 'pages#guides'
   get '/keyword_first', to: 'pages#keyword_first'
   get '/taiwan_departments', to: 'pages#taiwan_departments'
